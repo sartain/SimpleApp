a5f99618fee27dc67c6a6029b367e82ba289ca5f
sartain <alex.sartain@zuhlke.com>
2021-10-08 13:24:17 +0100

    First commit w/ignore

---
+++ TemplateProjectUITests/TemplateProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TemplateProjectUITestsLaunchTests.swift
+//  TemplateProjectUITests
+//
+//  Created by Alex Sartain on 08/10/2021.
+//
+
+import XCTest
+
+class TemplateProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
